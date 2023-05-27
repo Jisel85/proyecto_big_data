59b2383dd06e5129a00ab556a3d945b677664058
Omaira85 <ladyotavo@gmail.com>
2023-05-26 21:03:30 -0500

    feat: insertar información a mongo

--- funciones/conectar_bd.py
+++ funciones/conectar_bd.py
@@ -4,5 +4,7 @@ from pymongo import MongoClient
 def conectar_bd():
     connection_str = os.getenv('CREDENCIALES_BD')
     client = MongoClient(connection_str)
+    db = client["proyecto"]
+    collection = db["sentencias"]
     print(client.server_info())
-    return client
+    return collection
